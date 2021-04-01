87fea62b060c65b88c1e2420bba0326bc03fa4c0
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2021-04-01 10:40:40 -0400

    added find duplicates

--- arrays/challenges/FindDuplicates.cpp
+++ arrays/challenges/FindDuplicates.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+vector<int>* find_duplicates(int *arr, int len)
+{
+    vector<int> *duplicates = new vector<int>;
+    bool found_duplicate = false;
+    int last_duplicate_found = 0;
+    for (int i = 0; i < len-1; i++)
+    {
+        if (arr[i] == arr[i+1]) // we have a duplicate
+        {
+            if (found_duplicate == false)  // this is the first duplicate we have seen
+            {
+                found_duplicate = true;
+                last_duplicate_found = arr[i];
+                duplicates->push_back(arr[i]);
+            }
+            else  // we have seen a duplicate already
+            {
+                if (last_duplicate_found != arr[i])  // we only want one copy of each duplicate
+                {
+                    last_duplicate_found = arr[i];
+                    duplicates->push_back(arr[i]);
+                }
+            }
+        }
+    }
+    return duplicates;
+}
 
 
 int main()
 {
-
+    int test[19] = {-34, -31, -31, -5, -5, -1, 0, 0, 0, 1, 1, 6, 7, 88, 88, 200, 244, 555, 555};
+    vector<int> *duplicates = find_duplicates(test, 19);
+    int *arr = duplicates->data();
+    int n = duplicates->size();
+    cout << "Duplicates: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 }
--- arrays/challenges/FindMissingElement.cpp
+++ arrays/challenges/FindMissingElement.cpp
@@ -44,12 +44,31 @@ class FindMissingElement
             }
         return missing; // will return vector of mising elements
     }
+    /* Method using a hashset. Only works with positive nums, and max num cannot be too large */
+    static vector<int>* hash_method(int *arr, int len)
+    {
+        vector<int> *missing = new vector<int>;
+        // first find max item
+        int maximum = arr[0];
+        for (int i = 0; i < len; i++)
+            maximum = max(maximum, arr[i]);
+        // create an array of maximum element size
+        int hash[maximum+1];
+        // populate hash array
+        for (int i = 0; i < len; i++)
+            hash[arr[i]] = 1;
+        // now find the missing elements of the hash array
+        for (int i = 1; i <= maximum; i++)
+            if (hash[i] != 1)
+                missing->push_back(i);
+        return missing;
+    }
 };
 
 int main()
 {
-    int test[] = {1,2,3,4,5,6,8,10,13};
-    vector<int> *missing = FindMissingElement::index_method(test, 9);
+    int test[] = {1,2,3,4,5,6,8,10,13,16,19,22};
+    vector<int> *missing = FindMissingElement::hash_method(test, 12);
     int *res = missing->data();
     int len = missing->size();
     cout << "Missing elements: ";
