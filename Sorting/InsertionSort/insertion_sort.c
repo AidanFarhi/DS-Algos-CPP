6b14c9f1e14f642c39f616986232e34fea3619b0
AidanFarhi <aidanmcfarhi@gmail.com>
2021-05-06 18:36:25 -0400

    added boilerplate for Insertion Sort

---
+++ Sorting/InsertionSort/insertion_sort.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+/* Function Prototypes */
+void insertion_sort(int *arr, int n);
+void swap(int *arr, int i, int j);
+void print_array(int *arr, int n);
+
+/**
+ *   -----------
+ *   Insertion Sort
+ *   -----------
+ * 
+ *   - 
+ * 
+ *   Time Complexity
+ *   ---------------
+ *   Worst Case - O(N^2)
+ *   Best Case  - 
+ */
+
+void insertion_sort(int *arr, int n)
+{
+    
+}
+
+void swap(int *arr, int i, int j)
+{
+    int t = arr[i];
+    arr[i] = arr[j];
+    arr[j] = t;
+}
+
+void print_array(int *arr, int n)
+{
+    printf("[");
+    for (int i = 0; i < n; i++)
+    {
+        if (i == n-1) printf("%d", arr[i]);
+        else printf("%d, ", arr[i]);
+    }
+    printf("]\n");
+}
+
+int main()
+{
+    int test1[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int test2[] = {54, 2, -55, 22, 646, 2, 665, 76, 144, 7, 445, -22, -661, -6, 63, 43, 222};
+    bubble_sort(test1, 8);
+    bubble_sort(test2, 17);
+    print_array(test1, 8);
+    print_array(test2, 17);
+}
