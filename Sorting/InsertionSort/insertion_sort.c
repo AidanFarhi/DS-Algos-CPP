12a73f15fa24d7ff423d78c585334b231456b592
AidanFarhi <aidanmcfarhi@gmail.com>
2021-05-07 17:14:40 -0400

    added insertion sort

--- Sorting/InsertionSort/insertion_sort.c
+++ Sorting/InsertionSort/insertion_sort.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
 
-#include <stdio.h>
-
 /* Function Prototypes */
 void insertion_sort(int *arr, int n);
-void swap(int *arr, int i, int j);
 void print_array(int *arr, int n);
+void swap(int *arr, int i, int j);
 
 /**
- *   -----------
+ *   --------------
  *   Insertion Sort
- *   -----------
- * 
- *   - 
+ *   --------------
+ *   - Adaptive
+ *   - Stable  
  * 
  *   Time Complexity
  *   ---------------
  *   Worst Case - O(N^2)
- *   Best Case  - 
+ *   Best Case  - O(N) 
  */
 
 void insertion_sort(int *arr, int n)
 {
-    
-}
-
-void swap(int *arr, int i, int j)
-{
-    int t = arr[i];
-    arr[i] = arr[j];
-    arr[j] = t;
+    for (int i = 1; i < n; i++)
+    {
+        int current = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > current)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = current;
+    }
 }
 
 void print_array(int *arr, int n)
@@ -47,8 +48,8 @@ int main()
 {
     int test1[] = {1, 2, 3, 4, 5, 6, 7, 8};
     int test2[] = {54, 2, -55, 22, 646, 2, 665, 76, 144, 7, 445, -22, -661, -6, 63, 43, 222};
-    bubble_sort(test1, 8);
-    bubble_sort(test2, 17);
+    insertion_sort(test1, 8);
+    insertion_sort(test2, 17);
     print_array(test1, 8);
     print_array(test2, 17);
 }
